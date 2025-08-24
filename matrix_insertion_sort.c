a4ce7e4488c20da990156c3cc236db2cbad741d6
ohmyandri <120071660+ohmyandri@users.noreply.github.com>
2025-08-24 15:37:06 -0600

    Update matrix_insertion_sort.c
    Añadi los cambios, incluyendo la primera solucion en la cual se ordenaba renglon a renglon, y ademas la solucion completa en la cual se ordena todos los elementos haciendo que el elemento (0,0) sea el menor, y el (2,2) el mayor

--- matrix_insertion_sort.c
+++ matrix_insertion_sort.c
@@ -24,15 +24,75 @@ void sort_matrix(int **matrix, int n) {
     // TODO: Implementa aquí el algoritmo.
     // Necesitarás el método de inserción,
     // pero recuerda aplicar la regla de mover toda la columna.
+
+    //Primera implementacion aplicada:
+    /*Esta implementacion sirve pero lo que hace es ordenar renglon a renglon, y no lo solicitado
+    que fue ordenar TODA la matriz
+
+    int i, key, j, k;
+    for (i = 0; i < n; i++) {
+        for(j = 1; j < n; j++){
+            key = matrix[i][j];
+            k = j - 1;
+
+            while (k >= 0 && matrix[i][k] > key) {
+                matrix[i][k + 1] = matrix[i][k];
+                k = k - 1;
+            }
+            matrix[i][k + 1] = key;
+        }
+    }
+    */
+}
+
+void copy_sort_repeat(int **matrix, int *new_matrix, int n){
+    //El numero de elementos de una matriz de tamaño n x n, es la operacion n*n
+    int num_elementos = n * n;
+
+    //Copiando los elementos orig en nueva "matriz", ya que ahora es un arreglo 1D
+    int i, j, k = 0, key;
+    for(i = 0; i < n; i++){
+        for (j = 0; j < n; j++){
+            new_matrix[k] = matrix[i][j];
+            k++;
+        }
+    }
+
+    // Ordenando el arreglo unidimensional
+    for (i = 1; i < num_elementos; i++) {
+        key = new_matrix[i];
+        j = i - 1; 
+
+        while (j >= 0 && new_matrix[j] > key) {
+            new_matrix[j + 1] = new_matrix[j];
+            j = j - 1;
+        }
+        new_matrix[j + 1] = key;
+    }
+
+    //Reiniciando k para poder volverlo a utilizar
+    k = 0;
+
+    //Una vez ordenado, lo copiaremos nuevamente a la matriz original
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = new_matrix[k++];
+        }
+    }
 }
 
 int main() {
     int n = 3;
-    // Reserva dinámica de la matriz
+    // Reserva dinámica de la matriz 1
     int **matrix = (int **)malloc(n * sizeof(int *));
     for (int i = 0; i < n; i++) {
         matrix[i] = (int *)malloc(n * sizeof(int));
     }
+
+    // Decidi copiar toda la informacion de la matriz original a un arreglo simple con tal de
+    // Ordenarlo completamente ahi, para despues meterlo de vuelta a la matriz dada
+    // Reserva dinámica del arreglo unidimensional
+    int *new_matrix = (int *)malloc(n * n * sizeof(int));
 
     // Ejemplo de entrada
     int ejemplo[3][3] = {
@@ -50,14 +110,15 @@ int main() {
 
     printf("Matriz original:\n");
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {   
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 
     // Llamada a la función que deben completar
-    sort_matrix(matrix, n);
+    copy_sort_repeat(matrix, new_matrix, n);
+    //sort_matrix(matrix, n);
 
     printf("\nMatriz ordenada:\n");
     for (int i = 0; i < n; i++) {
@@ -67,11 +128,14 @@ int main() {
         printf("\n");
     }
 
-    // Liberar memoria
+    // Liberar memoria de mi primera reserva
     for (int i = 0; i < n; i++) {
         free(matrix[i]);
     }
     free(matrix);
+
+    // Liberar memoria de mi segunda reserva, que como es un arreglo !d, basta con usar free
+    free(new_matrix);
 
     return 0;
 }
